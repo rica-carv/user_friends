9124d59efc9fd2bff59092e8d06b67caa03f4438
rica-carv <rica-carv@users.noreply.github.com>
2026-05-01 14:14:00 +0100

    Add files via upload

--- languages/English/English_front.php
+++ languages/English/English_front.php
@@ -3,7 +3,7 @@
 //---- FINAL LANS, BASICALLY ONLY THE ONES USED IN USER_FRIENDS... ---
 //-----------------------------------------------------------------------
 // User profile LANs
-
+define("LAN_USERFRIEND_1", "This user does not have any friends yet. Please check back later.");
 define("LAN_USERFRIEND_2", "Friends of [x]");
 define("LAN_USERFRIEND_3", "Invalid ID");
 define("LAN_USERFRIEND_4", "My friends");
@@ -18,12 +18,29 @@ define("LAN_USERFRIEND_12", "Friend request pending");
 define("LAN_USERFRIEND_13", "Request unavailable"); // When rejected or deleted
 define("LAN_USERFRIEND_14", "Accept request");
 define("LAN_USERFRIEND_15", "Reject request");
-
+define("LAN_USERFRIEND_16", "The friends system has been temporarily disabled by the administrator.<br>
+Current friends remain visible, but any changes to your settings will only be applied when the system is reactivated.");
+define("LAN_USERFRIEND_17", "This user has chosen not to display their friends list.");
+/*
+Friends system is currently disabled by the site administrator.
+Your existing friends are still visible but settings cannot be modified.
+*/
+define("LAN_USERFRIEND_20", "Allow other users to send you friend requests?");
+define("LAN_USERFRIEND_21", "Automatically accept friend requests?");
+define("LAN_USERFRIEND_22", "Notification method for new friend requests?");
+define("LAN_USERFRIEND_23", "If no option is enabled, you will not be notified about new friend requests.");
+define("LAN_USERFRIEND_24", "Set profile visibility");
+define("LAN_USERFRIEND_25", "Public");
+define("LAN_USERFRIEND_26", "Friends");
+define("LAN_USERFRIEND_27", "Private");
+define("LAN_USERFRIEND_28", "Email");
+define("LAN_USERFRIEND_29", "Private Message");
 define("LAN_USERFRIEND_30", "Friend request accepted");
 define("LAN_USERFRIEND_31", "Friend removed");
 define("LAN_USERFRIEND_32", "Friend request removed");
 define("LAN_USERFRIEND_33", "Friend request rejected");
-
 define("LAN_USERFRIEND_34", "No friends yet");
 define("LAN_USERFRIEND_35", "No friend requests sent yet");
 define("LAN_USERFRIEND_36", "No friend requests received yet");
+define("LAN_USERFRIEND_37", "You have [x] sent friend request(s).");
+define("LAN_USERFRIEND_38", "You have [x] received friend request(s).");
--- languages/English/English_global.php
+++ languages/English/English_global.php
@@ -1,3 +1,5 @@
 <?php
 define("LAN_USERFRIENDS_FULLNAME", "Friends");
 define("LAN_USERFRIENDS_DESC", "Complete friends management. (based on several older plugins)");
+define("LAN_USERFRIENDS_MANAGE", "Manage Friends");
+define("LAN_USERFRIENDS_TOTAL", "total");
